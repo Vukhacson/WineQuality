b50cb71deb0804f5b7bd6fdd5017c152e441bed1
ScaleMind <scalemind@gmail.com>
2022-08-25 21:56:57 +0700

    Refresh

--- 5LittleBees/Data loader/Linh_DataLoader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-path1 = 'Data set/winequality-red.csv'
-path2 = 'Data set/winequality-white.csv'
-red_df = pd.read_csv(path1)
-white_df = pd.read_csv(path2)
-print(red_df.head())
-print(white_df.head())
